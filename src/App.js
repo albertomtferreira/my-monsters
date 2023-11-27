44a49d79982cd3362dbb660da88eed897d0e3856
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-27 22:40:16 +0000

    lesson 61
    lesson 61

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import {Component} from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import MESSAGES from './constants/constants';
 
-
-class App extends Component{
-  constructor(){
-    super();
-    this.state={
-      monsters:[],
-      searchField:''
-    }
-  }
-
-  componentDidMount(){
+const App = () => {
+  const [searchField, setSearchField] = useState(''); //[value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  
+  useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((users)=>this.setState(()=>{
-        return {monsters: users}
-        })
-      )
-      .catch((error)=>{
-        throw(`${MESSAGES.ERROR_MESSAGE}`,error)
-      })
-  }
-
-  onSearchChange = (event)=>{
-    const searchField = event.target.value.toLocaleLowerCase()
-    this.setState(()=>{
-      return{searchField}
+    .then((res) => res.json())
+    .then((users)=>setMonsters(users))
+    .catch((error)=>{
+      throw(`${MESSAGES.ERROR_MESSAGE}`,error)
     })
-  }
+  },[]);
 
-  render(){
-    const {monsters, searchField} = this.state
-    const {onSearchChange} = this
-    const filteredMonsters=monsters.filter((monster)=>{
+  useEffect(()=>{
+    const newFilteredMonsters=monsters.filter((monster)=>{
       return monster.name.toLocaleLowerCase().includes(searchField)
-    })
-    return [
-      <div key={'AppCaller'} className="App">
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  },[monsters,searchField])
+  
+  const onSearchChange = (event)=>{
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
+
+  return[
+    <div key={'AppCaller'} className="App">
         <h1 className='app-title'>Monsters</h1>
         <SearchBox
           className='monsters-search-box'
@@ -51,10 +42,7 @@ class App extends Component{
           monsters={filteredMonsters}
         />
       </div>
-      
-    ];
-    
-  }
+  ]
 }
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -9,12 +9,12 @@ class Card extends Component{
       <div className='card-container' key={id}>
               <img 
                 alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                src={`https://robohash.org/${id}?set=set3&size=180x180`}
               />
               <div className='card-text'>
-                <h2>{name}</h2>
-                <h3>{username}</h3>
-                <p>{email}</p>
+                <div className='inline-card'><h6 className='labels-card'>Name:</h6><h2>{name}</h2></div>
+                <div className='inline-card'><h6 className='labels-card'>Username:</h6><h3>{username}</h3></div>
+                <div className='inline-card'><h6 className='labels-card'>email:</h6><p>{email}</p></div>
               </div>
             </div>
     )
